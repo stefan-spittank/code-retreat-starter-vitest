f17546c034d9a7b088d544e5becc34ad10de82ca
Stefan Spittank <stefan.spittank@codecentric.de>
2024-11-22 09:55:08 +0100

    wip

--- game_of_live.spec.ts
+++ game_of_live.spec.ts
@@ -1,7 +1,14 @@
 import { describe, expect, it } from "vitest";
+import {cell} from "./gameOfLive";
 
 describe("Game Of Live", () => {
   it("should run a test", () => {
     expect(1).toBe(1);
   });
+
+  it("a cell should have a living state", ()=>{
+    expect(cell.state).toBe("living")
+  });
+
+  
 });
